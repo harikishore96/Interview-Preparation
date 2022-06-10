32c209bb7f5a5d477968c6845c2518b4d17c34b1
Hari Kishore <harikishore96@gmail.com>
2022-06-10 12:48:05 +0530

    UPDATE: Two Sum

--- src/main/java/com/silentanonym/interviewprep/arrays/TwoSum.java
+++ src/main/java/com/silentanonym/interviewprep/arrays/TwoSum.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
  */
 public class TwoSum {
 
+    // Runtime: 8 ms
+    // Memory : 45.4 MB
     public int[] twoSum(int[] nums, int target) {
         int[] output = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -21,5 +23,19 @@ public class TwoSum {
             }
         }
         return output;
+    }
+
+    // Runtime: 1 ms
+    // Memory : 42.1 MB
+    public int[] twoSumOnepassHashTable(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int index = 0; index < nums.length; index++) {
+            int diff = target - nums[index];
+            if(map.containsKey(diff)) {
+                return new int[] { map.get(diff), index };
+            }
+            map.put(nums[index], index);
+        }
+        return null;
     }
 }
