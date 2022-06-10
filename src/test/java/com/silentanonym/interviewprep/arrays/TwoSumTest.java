0a38e7522f7da26b5db05f994ed9a27059dc328f
Hari Kishore <harikishore96@gmail.com>
2022-06-10 12:50:57 +0530

    UPDATE: Two Sum

--- src/test/java/com/silentanonym/interviewprep/arrays/TwoSumTest.java
+++ src/test/java/com/silentanonym/interviewprep/arrays/TwoSumTest.java
@@ -19,4 +19,16 @@ public class TwoSumTest {
         actual = twoSum.twoSum(new int[] { 3, 3 }, 6);
         assertArrayEquals(new int[] { 0, 1 }, actual);
     }
+
+    @Test
+    void testTwoSumOnepassHashTable() {
+        int[] actual = twoSum.twoSumOnepassHashTable(new int[] { 2, 7, 11, 15 }, 9);
+        assertArrayEquals(new int[] { 0, 1 }, actual);
+
+        actual = twoSum.twoSumOnepassHashTable(new int[] { 3, 2, 4 }, 6);
+        assertArrayEquals(new int[] { 1, 2 }, actual);
+
+        actual = twoSum.twoSumOnepassHashTable(new int[] { 3, 3 }, 6);
+        assertArrayEquals(new int[] { 0, 1 }, actual);
+    }
 }
