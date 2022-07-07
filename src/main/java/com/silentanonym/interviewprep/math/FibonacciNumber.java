dc351fc1bdf168335258eb3d2ff85187e6c884e3
Hari Kishore <harikishore96@gmail.com>
2022-07-07 19:12:54 +0530

    UPDATE: Fibonacci Number

--- src/main/java/com/silentanonym/interviewprep/math/FibonacciNumber.java
+++ src/main/java/com/silentanonym/interviewprep/math/FibonacciNumber.java
@@ -1,18 +1,51 @@
-package com.silentanonym.interviewprep.linkedlist;
+package com.silentanonym.interviewprep.math;
 
 public class FibonacciNumber {
 
-    public int fib(int n) {
-        return fib(n, new int[31]);
+  // Dynamic Programming - Iteration & Tabulation/Memoization
+  // Time : O(N), Space : O(N)
+  public int fib1(int n) {
+    if (n <= 1) {
+      return n;
     }
 
-    private int fib(int n, int[] map) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-
-        if (map[n] == 0) {
-            map[n] = fib(n - 1, map) + fib(n - 2, map);
-        }
-        return map[n];
+    int[] dp = new int[n + 1];
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
     }
+    return dp[n];
+  }
+
+  // Dynamic Programming - Iteration
+  // Time : O(N), Space : O(1)
+  public int fib2(int n) {
+    if (n <= 1) {
+      return n;
+    }
+    int prev2 = 0, prev = 1;
+    for (int i = 2; i <= n; i++) {
+      int curr = prev2 + prev;
+      prev2 = prev;
+      prev = curr;
+    }
+    return prev;
+  }
+
+  // Dynamic Programming - Recursion & Memoization
+  // Time : O(N), Space : O(N*N) - N for Recursion Call stack, N for input
+  public int fib(int n) {
+    return fib(n, new int[31]);
+  }
+
+  private int fib(int n, int[] map) {
+    if (n <= 1) {
+      return n;
+    }
+
+    if (map[n] == 0) {
+      map[n] = fib(n - 1, map) + fib(n - 2, map);
+    }
+    return map[n];
+  }
 }
--- src/test/java/com/silentanonym/interviewprep/math/FibonacciNumberTest.java
+++ src/test/java/com/silentanonym/interviewprep/math/FibonacciNumberTest.java
@@ -1,16 +1,36 @@
-package com.silentanonym.interviewprep.linkedlist;
+package com.silentanonym.interviewprep.math;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class FibonacciNumberTest {
 
-    FibonacciNumber fibonacciNumber = new FibonacciNumber();
+  FibonacciNumber fibonacciNumber = new FibonacciNumber();
 
-    @Test
-    void fib() {
-        Assertions.assertEquals(1, fibonacciNumber.fib(2));
-        Assertions.assertEquals(2, fibonacciNumber.fib(3));
-        Assertions.assertEquals(3, fibonacciNumber.fib(4));
-    }
+  @Test
+  void fib1() {
+    Assertions.assertEquals(0, fibonacciNumber.fib1(0));
+    Assertions.assertEquals(1, fibonacciNumber.fib1(1));
+    Assertions.assertEquals(1, fibonacciNumber.fib1(2));
+    Assertions.assertEquals(2, fibonacciNumber.fib1(3));
+    Assertions.assertEquals(3, fibonacciNumber.fib1(4));
+    Assertions.assertEquals(55, fibonacciNumber.fib1(10));
+  }
+
+  @Test
+  void fib2() {
+    Assertions.assertEquals(0, fibonacciNumber.fib2(0));
+    Assertions.assertEquals(1, fibonacciNumber.fib2(1));
+    Assertions.assertEquals(1, fibonacciNumber.fib2(2));
+    Assertions.assertEquals(2, fibonacciNumber.fib2(3));
+    Assertions.assertEquals(3, fibonacciNumber.fib2(4));
+    Assertions.assertEquals(55, fibonacciNumber.fib2(10));
+  }
+
+  @Test
+  void fib() {
+    Assertions.assertEquals(1, fibonacciNumber.fib(2));
+    Assertions.assertEquals(2, fibonacciNumber.fib(3));
+    Assertions.assertEquals(3, fibonacciNumber.fib(4));
+  }
 }
