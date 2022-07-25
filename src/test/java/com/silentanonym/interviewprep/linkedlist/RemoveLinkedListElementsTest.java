3d5cf1893c57eb44f73f3a29d7388cb43a141c04
Hari Kishore <harikishore96@gmail.com>
2022-07-25 22:47:01 +0530

    UPDATE: 203. Remove Linked List Elements (Easy)

--- src/test/java/com/silentanonym/interviewprep/linkedlist/RemoveLinkedListElementsTest.java
+++ src/test/java/com/silentanonym/interviewprep/linkedlist/RemoveLinkedListElementsTest.java
@@ -1,31 +1,30 @@
 package com.silentanonym.interviewprep.linkedlist;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class RemoveLinkedListElementsTest {
 
-    RemoveLinkedListElements removeLinkedListElements = new RemoveLinkedListElements();
+  RemoveLinkedListElements removeLinkedListElements = new RemoveLinkedListElements();
 
-    @Test
-    void removeElements() {
+  @Test
+  void removeElements() {
 
-        // [1,2,6,3,4,5,6], val = 6
-        RemoveLinkedListElements.ListNode head = removeLinkedListElements.new ListNode(1);
-        head.next(2).next(6).next(3).next(4).next(5).next(6);
+    // Test Case 1
+    ListNode head = new ListNode(1);
+    head.next(2).next(6).next(3).next(4).next(5).next(6);
+    ListNode actual = removeLinkedListElements.removeElements(head, 6);
+    Assertions.assertArrayEquals(new int[]{1, 2, 3, 4, 5}, ListNode.toArray(actual));
 
-        RemoveLinkedListElements.ListNode expected = removeLinkedListElements.new ListNode(1);
-        expected.next(2).next(3).next(4).next(5);
-        print(head);
+    // Test Case 2
+    head = null;
+    actual = removeLinkedListElements.removeElements(head, 1);
+    Assertions.assertArrayEquals(new int[]{}, ListNode.toArray(actual));
 
-        RemoveLinkedListElements.ListNode actual = removeLinkedListElements.removeElements(head, 6);
-        print(actual);
-    }
-
-    private void print(RemoveLinkedListElements.ListNode head) {
-        System.out.println("");
-        while (head != null) {
-            System.out.print(head.val);
-            head = head.next;
-        }
-    }
+    // Test Case 3
+    head = new ListNode(7);
+    head.next(7).next(7).next(7);
+    actual = removeLinkedListElements.removeElements(head, 7);
+    Assertions.assertArrayEquals(new int[]{}, ListNode.toArray(actual));
+  }
 }
