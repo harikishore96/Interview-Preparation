99d9a57c88f157d67dc9318f9c28de4fe1671c37
Hari Kishore <harikishore96@gmail.com>
2022-07-26 22:55:30 +0530

    NEW: 430. Flatten a Multilevel Doubly Linked List (Medium)

---
+++ src/main/java/com/silentanonym/interviewprep/linkedlist/FlattenAMultilevelDoublyLinkedList.java
@@ -0,0 +1,39 @@
+package com.silentanonym.interviewprep.linkedlist;
+
+public class FlattenAMultilevelDoublyLinkedList {
+
+  public Node flatten(Node head) {
+    if (head == null) {
+      return null;
+    }
+    Node dummy = head;
+    while (dummy != null) {
+      if (dummy.child != null) {
+        Node next = dummy.next;
+        Node child = flatten(dummy.child);
+        dummy.child = null;
+        dummy.next = child;
+        child.prev = dummy;
+        while (child.next != null) {
+          child = child.next;
+        }
+        child.next = next;
+        if (next != null) {
+          next.prev = child;
+        }
+        dummy = child;
+      }
+      dummy = dummy.next;
+    }
+    return head;
+  }
+}
+
+// Definition for a Node.
+class Node {
+
+  public int val;
+  public Node prev;
+  public Node next;
+  public Node child;
+}
